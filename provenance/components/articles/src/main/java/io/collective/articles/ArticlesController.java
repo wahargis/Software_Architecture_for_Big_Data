db44c507a13b1355b558674f34925fac33519f45
wahargis <38849002+wahargis@users.noreply.github.com>
2024-01-07 00:01:07 -0800

    Update ArticlesController.java
    added ArticlesController implementation that passes build tests.
    
    Need to confirm that write output is JSON; exercise notes mentions a writeJsonBody method.

--- provenance/components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ provenance/components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -6,14 +6,19 @@ import org.eclipse.jetty.server.Request;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArticlesController extends BasicHandler {
     private final ArticleDataGateway gateway;
+    private final ObjectMapper mapper;
 
     public ArticlesController(ObjectMapper mapper, ArticleDataGateway gateway) {
         super(mapper);
         this.gateway = gateway;
+        this.mapper = mapper;
     }
 
     @Override
@@ -21,15 +26,62 @@ public class ArticlesController extends BasicHandler {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
+                
+                // query the gateway for all articles which are returned as a list of class ArticleRecord
+                List<ArticleRecord> articles = gateway.findAll();
 
+                // stream the articles and map the contents to list of ArticleInfo class
+                List<ArticleInfo> articleInfos = articles.stream()
+                    .map(article -> new ArticleInfo(article.getId(), article.getTitle()))
+                    .collect(Collectors.toList());
+                
+                // send collection of article infos; write the articleInfos to a servlet http response
+                writeResponse(servletResponse, articleInfos);
             }
+
         });
 
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
 
+                // query the gateway for available articles which are returned as a list of class ArticleRecord
+                List<ArticleRecord> articles = gateway.findAvailable();
+
+                // stream the articles and map the contents to list of ArticleInfo class
+                List<ArticleInfo> articleInfos = articles.stream()
+                    .map(article -> new ArticleInfo(article.getId(), article.getTitle()))
+                    .collect(Collectors.toList());
+
+                // send collection of article infos; write the articleInfos to a servlet http response
+                writeResponse(servletResponse, articleInfos);
+
             }
+
         });
     }
+
+    private void writeResponse(HttpServletResponse response, List<ArticleInfo> articleInfos) {
+        //Description:
+        //   a helper method for writing an http response of our articleInfos
+        //Parameters:
+        //   response | HttpServletResponse | an http response from the javax servlet package
+        //   articles | List<ArticleInfo> | a list of ArticleInfos containing article ID and title
+        //Return:
+        //   void
+        try {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_OK);
+            mapper.writeValue(response.getOutputStream(), articleInfos);
+        } catch (IOException e) {
+            // Log the exception for debugging purposes
+            // log.error("Error writing response: " + e.getMessage(), e);
+            
+            // for the sake of the exercise, we will simply print an error statement to system out
+            System.out.println("An IOException was caught in writeResponse");
+
+            // Set an appropriate HTTP error status
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    };
 }
